e900c42affd07c3ef770c0d6df8b072b3cac5481
Keesha Hargrove <kharg14@gmail.com>
2020-09-21 14:20:58 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,10 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
-  class CreateSongs < ActiveRecord::Migration[5.2]
-    def change
-        create_table :songs do |song|
-            song.string :name
-            song.integer :artist_id
-            song.integer :genre_id
+  def change
+        create_table :songs do |t|
+            t.string :name
+            t.integer :artist_id
+            t.integer :genre_id
         end
     end
 end
