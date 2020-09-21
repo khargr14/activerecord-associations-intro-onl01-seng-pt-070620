97d9a1b13d8610c019ccfc6bf8f9eacaa4732ef3
Keesha Hargrove <kharg14@gmail.com>
2020-09-21 14:20:07 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,8 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
-  class CreateArtists < ActiveRecord::Migration[5.2]
     def change
-        create_table :artists do |artist|
-            artist.string :name
+        create_table :artists do |t|
+            t.string :name
         end
     end
 end
