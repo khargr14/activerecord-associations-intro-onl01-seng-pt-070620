40e7a37f2bf640463ac82305d7e466c8747e2bba
Keesha Hargrove <kharg14@gmail.com>
2020-09-21 14:01:34 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,2 +1,8 @@
 class CreateGenres < ActiveRecord::Migration[4.2]
+  class CreateGenres < ActiveRecord::Migration[5.2]
+    def change
+        create_table :genres do |genre|
+            genre.string :name
+        end
+    end
 end
